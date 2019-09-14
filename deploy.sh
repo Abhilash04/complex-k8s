38529cf5ba1f670855d627cd538c3d3d1a72ffb4
Abhilash Sharma <abhilash.sharma@block8.io>
2019-09-15 03:33:26 +0530

    Title Change

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 kubectl apply -f k8s
-kubectl set image deployments/client-deployment server=$COMMIT_IMAGE_TAG_CLIENT
 kubectl set image deployments/server-deployment server=$COMMIT_IMAGE_TAG_SERVER
+kubectl set image deployments/client-deployment server=$COMMIT_IMAGE_TAG_CLIENT
 kubectl set image deployments/worker-deployment server=$COMMIT_IMAGE_TAG_WORKER
