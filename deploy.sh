ebd6eda71e89e5dea992d4600a0437bd6effed02
Abhilash Sharma <abhilash.sharma@block8.io>
2019-09-15 04:12:58 +0530

    Container Names

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,5 @@
 
 kubectl apply -f k8s
 kubectl set image deployments/server-deployment server=$COMMIT_IMAGE_TAG_SERVER
-kubectl set image deployments/client-deployment server=$COMMIT_IMAGE_TAG_CLIENT
-kubectl set image deployments/worker-deployment server=$COMMIT_IMAGE_TAG_WORKER
+kubectl set image deployments/client-deployment client=$COMMIT_IMAGE_TAG_CLIENT
+kubectl set image deployments/worker-deployment worker=$COMMIT_IMAGE_TAG_WORKER
