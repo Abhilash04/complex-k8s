c2f1dcaa8efd902a2c64c304c8e39dfe720ee7b2
Abhilash Sharma <abhilash.sharma@block8.io>
2019-09-15 01:08:58 +0530

    change in docker images

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,12 @@
-docker build -t $COMMIT_IMAGE_TAG_CLIENT - t $BRANCH_IMAGE_TAG_CLIENT -f ./client/Dockerfile ./client
-docker build -t $COMMIT_IMAGE_TAG_SERVER -t $BRANCH_IMAGE_TAG_SERVER -f ./server/Dockerfile ./server
-docker build -t $COMMIT_IMAGE_TAG_WORKER -t $BRANCH_IMAGE_TAG_WORKER -f ./worker/Dockerfile ./worker
+docker build -t $COMMIT_IMAGE_TAG_CLIENT - t $LATEST_IMAGE_TAG_CLIENT -f ./client/Dockerfile ./client
+docker build -t $COMMIT_IMAGE_TAG_SERVER -t $LATEST_IMAGE_TAG_SERVER -f ./server/Dockerfile ./server
+docker build -t $COMMIT_IMAGE_TAG_WORKER -t $LATEST_IMAGE_TAG_WORKER -f ./worker/Dockerfile ./worker
 docker push $COMMIT_IMAGE_TAG_CLIENT
-docker push $BRANCH_IMAGE_TAG_CLIENT
+docker push $LATEST_IMAGE_TAG_CLIENT
 docker push $COMMIT_IMAGE_TAG_SERVER
-docker push $BRANCH_IMAGE_TAG_SERVER
+docker push $LATEST_IMAGE_TAG_SERVER
 docker push $COMMIT_IMAGE_TAG_WORKER
-docker push $BRANCH_IMAGE_TAG_WORKER
+docker push $LATEST_IMAGE_TAG_WORKER
 kubectl apply -f k8s
 kubectl set image deployments/client-deployment server=$COMMIT_IMAGE_TAG_CLIENT
 kubectl set image deployments/server-deployment server=$COMMIT_IMAGE_TAG_SERVER
