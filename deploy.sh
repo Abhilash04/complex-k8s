07ed17738e8ea2f56eb311ce46be9c24f627015a
Abhilash Sharma <abhilash.sharma@block8.io>
2019-09-15 00:48:23 +0530

    gcloud docker config

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,12 @@
-gcloud docker build -t $COMMIT_IMAGE_TAG_CLIENT - t $BRANCH_IMAGE_TAG_CLIENT -f ./client/gcloud Dockerfile ./client
-gcloud docker build -t $COMMIT_IMAGE_TAG_SERVER -t $BRANCH_IMAGE_TAG_SERVER -f ./server/gcloud Dockerfile ./server
-gcloud docker build -t $COMMIT_IMAGE_TAG_WORKER -t $BRANCH_IMAGE_TAG_WORKER -f ./worker/gcloud Dockerfile ./worker
-gcloud docker push $COMMIT_IMAGE_TAG_CLIENT
-gcloud docker push $BRANCH_IMAGE_TAG_CLIENT
-gcloud docker push $COMMIT_IMAGE_TAG_SERVER
-gcloud docker push $BRANCH_IMAGE_TAG_SERVER
-gcloud docker push $COMMIT_IMAGE_TAG_WORKER
-gcloud docker push $BRANCH_IMAGE_TAG_WORKER
+docker build -t $COMMIT_IMAGE_TAG_CLIENT - t $BRANCH_IMAGE_TAG_CLIENT -f ./client/Dockerfile ./client
+docker build -t $COMMIT_IMAGE_TAG_SERVER -t $BRANCH_IMAGE_TAG_SERVER -f ./server/Dockerfile ./server
+docker build -t $COMMIT_IMAGE_TAG_WORKER -t $BRANCH_IMAGE_TAG_WORKER -f ./worker/Dockerfile ./worker
+docker push $COMMIT_IMAGE_TAG_CLIENT
+docker push $BRANCH_IMAGE_TAG_CLIENT
+docker push $COMMIT_IMAGE_TAG_SERVER
+docker push $BRANCH_IMAGE_TAG_SERVER
+docker push $COMMIT_IMAGE_TAG_WORKER
+docker push $BRANCH_IMAGE_TAG_WORKER
 kubectl apply -f k8s
 kubectl set image deployments/client-deployment server=$COMMIT_IMAGE_TAG_CLIENT
 kubectl set image deployments/server-deployment server=$COMMIT_IMAGE_TAG_SERVER
